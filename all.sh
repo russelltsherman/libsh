894f362d4fa49296f58676af63fcb0bae3091a9f
Russell <russell@union-pos.com>
2018-11-21 09:47:10 -0600

    fix libsh source paths

--- all.sh
+++ all.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC1090
 
 LIBDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${LIBDIR}/banner.sh"
-source "${LIBDIR}/echos.sh"
-source "${LIBDIR}/functions.sh"
-source "${LIBDIR}/iterm.sh"
-source "${LIBDIR}/requirers.sh"
-source "${LIBDIR}/validate.sh"
-source "${LIBDIR}/validate_ip.sh"
+source "${LIBDIR}/lib/banner.sh"
+source "${LIBDIR}/lib/echos.sh"
+source "${LIBDIR}/lib/functions.sh"
+source "${LIBDIR}/lib/iterm.sh"
+source "${LIBDIR}/lib/requirers.sh"
+source "${LIBDIR}/lib/validate.sh"
+source "${LIBDIR}/lib/validate_ip.sh"
